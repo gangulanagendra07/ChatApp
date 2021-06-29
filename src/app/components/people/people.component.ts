038d3aa9ac9e0cfd86e870837d98ea7f4c4ed9b9
Nagendra <nagendra.gangula1991@gmail.com>
2021-06-29 16:28:16 +0530

    Added new service and some functionalities in Login and people components

--- src/app/components/people/people.component.ts
+++ src/app/components/people/people.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from 'src/app/services/users.service';
+import _ from 'lodash';
+import { TokenService } from 'src/app/services/token.service';
+import { ɵINTERNAL_BROWSER_PLATFORM_PROVIDERS } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-people',
@@ -9,17 +12,34 @@ import { UsersService } from 'src/app/services/users.service';
 export class PeopleComponent implements OnInit {
 
   users = [];
+  loggedInUser: any;
 
-  constructor(private userService: UsersService) { }
+  constructor(private userService: UsersService, private tokenService: TokenService) { }
 
   ngOnInit() {
+    this.loggedInUser = this.tokenService.GetPayload();
     this.GetUsers();
+    this.GetUser();
   }
 
   GetUsers(){
     this.userService.getAllUsers().subscribe(data => {
+      // Here we are using lodash method to remove loggedInUser from server response.
+      _.remove(data.result, {username: this.loggedInUser.username});
        this.users = data.result;
     });
+  }
+
+  GetUser(){
+    this.userService.getById(this.loggedInUser._id).subscribe(data => {
+      console.log(data);
+    });
+  }
+
+  FollowUser(user){
+    this.userService.followUser(user._id).subscribe(data =>{
+        console.log(data);
+    })
   }
 
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,19 +1,32 @@
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 
 const BASEURL = 'http://localhost:3000/api/users';
+const BASEURLFRIENDS = 'http://localhost:3000/api/friends';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor( private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  getAllUsers(): Observable<any>{
-     return this.http.get(`${BASEURL}/getusers`);
+  getAllUsers(): Observable<any> {
+    return this.http.get(`${BASEURL}/getusers`);
+  }
+
+  getById(id): Observable<any> {
+    return this.http.get(`${BASEURL}/getusers/${id}`);
+  }
+
+  getUserByName(username): Observable<any> {
+    return this.http.get(`${BASEURL}/getusers/${username}`);
+  }
+
+  followUser(userFollowed): Observable<any> {
+    return this.http.post(`${BASEURLFRIENDS}/friends`, { userFollowed });
   }
 
 }
